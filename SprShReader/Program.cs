5ee8930a58af77d39ffb56dd1d808040a4f93b1d
kchr2026 <kchr2026@gmail.com>
2026-03-19 10:30:23 +0100

    Cleanup

--- SprShReader/Program.cs
+++ SprShReader/Program.cs
@@ -13,39 +13,8 @@ class Program
         Ui ui = new Ui();
         ui.UI(digimons);
         ui.UserInterface();
-        
-      
 
         bool firstLine = true; 
-        // foreach (string line in File.ReadLines(path))
-        // {
-        //     if (firstLine) { firstLine = false; continue; }
-        //
-        //     string[] col = line.Split(',');
-        //
-        //     var digimon = new Digimon
-        //     {
-        //         Number     = int.Parse(col[0]),
-        //         Name       = col[1],
-        //         Stage      = col[2],
-        //         Type       = col[3],
-        //         Attribute  = col[4],
-        //         Memory     = int.Parse(col[5]),
-        //         EquipSlots = int.Parse(col[6]),
-        //         Hp         = int.Parse(col[7]),
-        //         Sp         = int.Parse(col[8]),
-        //         Atk        = int.Parse(col[9]),
-        //         Def        = int.Parse(col[10]),
-        //         Int        = int.Parse(col[11]),
-        //         Spd        = int.Parse(col[12]),
-        //     };
-        //
-        //     digimons.Add(digimon);
-        // }
-        // foreach (var d in digimons)
-        // {
-        //     Console.WriteLine($"{d.Name} | {d.Stage} | {d.Attribute} | Memory: {d.Memory} | EquipSlots: {d.EquipSlots} | HP: {d.Hp} | ATK: {d.Atk}");
-        // }
     return 0;
     }
 }
